c19bab67bc37e3ad37bf125e892cfc57ac44456f
Daniel Wyatt <daniel.wyatt@ribose.com>
2023-02-03 12:09:41 -0600

    Simplify library loading

--- lib/confium/lib.rb
+++ lib/confium/lib.rb
@@ -2,43 +2,6 @@ require 'ffi'
 
 module Confium
   module Lib
-    # FFI load pattern taken from: ffi-geos
-    # https://github.com/dark-panda/ffi-geos/blob/master/lib/ffi-geos.rb
-    extend FFI::Library
-
-    def self.search_paths
-      @search_paths ||= \
-        if ENV['CONFIUM_LIBRARY_PATH']
-          [ENV['CONFIUM_LIBRARY_PATH']]
-        elsif FFI::Platform::IS_WINDOWS
-          ENV['PATH'].split(File::PATH_SEPARATOR)
-        else
-          [
-            '/usr/local/{lib64,lib}',
-            '/opt/local/{lib64,lib}',
-            '/usr/{lib64,lib}',
-            '/opt/homebrew/lib',
-            '/usr/lib/{x86_64,i386,aarch64}-linux-gnu'
-          ]
-        end
-    end
-
-    def self.find_lib(lib)
-      if ENV['CONFIUM_LIBRARY_PATH'] && File.file?(ENV['CONFIUM_LIBRARY_PATH'])
-        ENV['CONFIUM_LIBRARY_PATH']
-      else
-        Dir.glob(search_paths.map do |path|
-          File.expand_path(File.join(path, "#{lib}.#{FFI::Platform::LIBSUFFIX}{,.?}"))
-        end).first
-      end
-    end
-
-    def self.confium_library_path
-      @confium_library_path ||= \
-        # On MingW the libraries have version numbers
-        find_lib('{lib,}confium{,-?}')
-    end
-
     extend ::FFI::Library
 
     FFI_LAYOUT = {
@@ -55,7 +18,7 @@ module Confium
       cfm_hash_destroy: [ %i[pointer], :void ],
     }.freeze
 
-    ffi_lib(confium_library_path)
+    ffi_lib(%w[confium libconfium])
 
     FFI_LAYOUT.each do |func, ary|
       begin
